b5033aee0ae75cea95ebe87228dd8edc624866b8
djum77777 <mariana.djum@yahoo.com>
2023-12-21 21:18:59 +0700

    challenge string

--- buatRepo.js
+++ buatRepo.js
@@ -1,2 +1,12 @@
 //latihan git
 console.log("hallo");
+
+//challenge manipulasi string
+let namaDepan = "Mariana";
+let namaBelakang = "Djum";
+let gabung = (`Halo nama saya ${namaDepan} ${namaBelakang}`)
+console.log(gabung);
+
+let data = "halo nama saya adit saya sedang belajar javascript"
+let ubah = data.replace(/saya/g, "aku")
+console.log(ubah);
